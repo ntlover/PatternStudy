407539625887b5c1f1b09ba60dfb27efdf6f9f82
ntlover <ntlover@naver.com>
2016-07-05 11:10:00 +0900

    Animal move() method add

--- pattern_lesson_01(strategy pattern)/src/pattern_lesson_01/Animal.java
+++ pattern_lesson_01(strategy pattern)/src/pattern_lesson_01/Animal.java
@@ -2,10 +2,10 @@ package pattern_lesson_01;
 
 public class Animal {
 
-//	public void move(){
-//		
-//		System.out.println("move move move");
-//		
-//	}
+	public void move(){
+		
+		System.out.println("move move move");
+		
+	}
 	
 }
